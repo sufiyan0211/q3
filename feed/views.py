49566d97d2cb0d3bd2100fc0b7987cd11292098e
sufiyan0211 <sufiyan0211@gmail.com>
2020-07-30 17:09:14 +0530

    2

---
+++ feed/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+import feedparser
+
+def index(request):
+    if request.GET.get("url"):
+        url = request.GET["url"] #Getting URL
+        feed = feedparser.parse(url) #Parsing XML data
+    else:
+        feed = None
+    return render(request, 'feed/reader.html', {'feed' : feed,})
